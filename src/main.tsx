61d744b3dcd3eed5f277fa6de734841ab65efc9f
TastyFuji <ajizflame@gmail.com>
2025-03-20 17:52:49 +0700

    Demo V2

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <BrowserRouter basename="/myTerminal">
     <App />
-  </StrictMode>,
+  </BrowserRouter>
 )
